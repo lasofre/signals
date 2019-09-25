a049125a6ca2a7fe1a5bf46fbb8dd84b524e79c2
Matias <matias_m_b@hotmail.com>
2019-09-25 00:45:25 -0300

    Se arreglo el integrador

--- integrador/Ejemplos/integrador.py
+++ integrador/Ejemplos/integrador.py
@@ -1,20 +1,45 @@
 # -*- coding: utf-8 -*-
+import array
 import math
 import matplotlib.pyplot as plt
-import wave
-obj = wave.open('in.wav','r')
-tm=1/obj.getframerate()
-N=obj.getnframes()
-muestr=list()
+from wave import open as waveOpen
+import struct
+s = waveOpen('in.wav','rb')
+(nc,sw,fr,nf,comptype, compname) = s.getparams( )
+print(nc,sw,fr,nf,comptype, compname)
+#data = s.readframes(nf)
+tm=float(1/fr)
 integral=0
-trama=0
-signal=list()
-signal=obj.rewind()
-for i in range(0,N):
-    integral+=float(signal[i]*tm)
-    muestr.append(integral)
+data = array.array('h',s.readframes(nf))
+s.close()
+muestr=array.array('h')
+sample=0
+g=1000
+for i in range(0,nf):
+    integral+=float(data[i]*tm)*g
+    muestr.append(int(integral))
+minimo=muestr[0]
+maximo=muestr[0]
+for i in range(0,nf):
+    if maximo<muestr[i]:
+        maximo=muestr[i]
+    if minimo>muestr[i]:
+        minimo=muestr[i]
+print(maximo-minimo)
+for i in range(0,nf):
+    if (maximo-minimo)>0 and maximo>0 and minimo>0:
+        muestr[i]=(muestr[i]-int((maximo-minimo)/2))
+    elif minimo<0 and maximo<0 and (minimo-maximo)<0:
+        muestr[i]=(muestr[i]-int((minimo-maximo)/2))
+    else:
+        muestr[i]=int(float(muestr[i])-(maximo+minimo)/nf*i)
+f = waveOpen('out.wav', 'w')
+f.setparams((nc, sw, fr, nf, "NONE", "Uncompressed"))
+f.writeframes(muestr.tostring())
+f.close()
 plt.subplot(211)
-plt.plot(tiemp,signal)
+plt.plot(data)
 plt.subplot(212)
-plt.plot(tiemp,muestr)
+plt.plot(muestr)
 plt.show()
+
